70a185160234a56840e4d8c43ae7ace1c67f3652
Álvaro Vargas Molina <36602950+AlvaroVargasM@users.noreply.github.com>
2020-10-01 21:56:42 -0700

    Added a default view of the page

--- fv-front-end/src/app/app.component.ts
+++ fv-front-end/src/app/app.component.ts
@@ -10,7 +10,7 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'fv-front-end';
-  shownView;
+  shownView = 'admin';
 
   showView(view: string){
     this.shownView = view;
