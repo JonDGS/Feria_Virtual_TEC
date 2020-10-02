e9f2de5aabaab254c7bf41d86cf63f1953ec91b9
Dxnium <38996231+Dxnium@users.noreply.github.com>
2020-10-01 22:14:04 -0600

    Header currentActivate

--- fv-front-end/src/app/header/header.component.ts
+++ fv-front-end/src/app/header/header.component.ts
@@ -1,19 +1,21 @@
-import {Component, OnInit, Output, EventEmitter} from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
   @Output() viewSelected = new EventEmitter<string>();
 
-  constructor() { }
+  constructor() {}
+  current = 'admin';
+  isActivate = false;
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  onSelectView(view: string){
+  onSelectView(view: string) {
     this.viewSelected.emit(view);
+    this.current = view;
   }
 }
