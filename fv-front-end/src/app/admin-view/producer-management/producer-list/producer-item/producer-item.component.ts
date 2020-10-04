9cb3d8336cc2f99c4f11aa76b49bf3d0dc8edb72
Álvaro Vargas Molina <36602950+AlvaroVargasM@users.noreply.github.com>
2020-10-03 21:01:33 -0700

    Added the main components for the admin view

---
+++ fv-front-end/src/app/admin-view/producer-management/producer-list/producer-item/producer-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-producer-item',
+  templateUrl: './producer-item.component.html',
+  styleUrls: ['./producer-item.component.css']
+})
+export class ProducerItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- fv-front-end/src/app/app.module.ts
+++ fv-front-end/src/app/app.module.ts
@@ -6,6 +6,13 @@ import { HeaderComponent } from './header/header.component';
 import { AdminViewComponent } from './admin-view/admin-view.component';
 import { ProducerViewComponent } from './producer-view/producer-view.component';
 import { ClientViewComponent } from './client-view/client-view.component';
+import { ProducerManagementComponent } from './admin-view/producer-management/producer-management.component';
+import { ProducerListComponent } from './admin-view/producer-management/producer-list/producer-list.component';
+import { ProducerDetailsComponent } from './admin-view/producer-management/producer-details/producer-details.component';
+import { ProducerItemComponent } from './admin-view/producer-management/producer-list/producer-item/producer-item.component';
+import { CategorieManagementComponent } from './admin-view/categorie-management/categorie-management.component';
+import { CategorieListComponent } from './admin-view/categorie-management/categorie-list/categorie-list.component';
+import { CategorieEditComponent } from './admin-view/categorie-management/categorie-edit/categorie-edit.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +20,14 @@ import { ClientViewComponent } from './client-view/client-view.component';
     HeaderComponent,
     AdminViewComponent,
     ProducerViewComponent,
-    ClientViewComponent
+    ClientViewComponent,
+    ProducerManagementComponent,
+    ProducerListComponent,
+    ProducerDetailsComponent,
+    ProducerItemComponent,
+    CategorieManagementComponent,
+    CategorieListComponent,
+    CategorieEditComponent
   ],
   imports: [
     BrowserModule
