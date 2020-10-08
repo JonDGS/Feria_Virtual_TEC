c3da508a251fa4985da938eaa16eebe4ed31a886
Álvaro Vargas Molina <36602950+AlvaroVargasM@users.noreply.github.com>
2020-10-07 23:06:14 -0700

    Added a list of categories and a way to add them to the list by an input text and button

--- fv-front-end/src/app/server.service.ts
+++ fv-front-end/src/app/server.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@angular/core';
+import {Category} from './models/category.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ServerService {
-  //producers requests array to save resquests injected by the form
+  // producers requests array to save resquests injected by the form
   producersRequestsObj: { id: number; name: string }[] = [];
+  // category list for storing the categories made by the user
+  categoryList: Category[] = [
+    new Category('verduras', 1),
+    new Category('frutas', 2)
+  ];
+  categoryID = 1;
 
   constructor() {}
 
@@ -15,5 +22,10 @@ export class ServerService {
 
   producerRequests() {
     return this.producersRequestsObj;
+  }
+
+  addCategory(category){
+    this.categoryList.push(category);
+    this.categoryID++;
   }
 }
