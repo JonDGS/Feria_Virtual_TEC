003e767fc2d9d492d5d370c52146a7c211b6d20e
Álvaro Vargas Molina <36602950+AlvaroVargasM@users.noreply.github.com>
2020-10-14 02:54:46 -0700

    Modified the form on the form-producer.component to make it possible to created a producer.model object from it

--- fv-front-end/src/app/producer-view/form-producer/form-producer.component.ts
+++ fv-front-end/src/app/producer-view/form-producer/form-producer.component.ts
@@ -1,5 +1,7 @@
 import { ServerService } from './../../server.service';
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {Producer} from '../../models/producer.model';
 
 @Component({
   selector: 'form-producer',
@@ -7,11 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-producer.component.css'],
 })
 export class FormProducerComponent implements OnInit {
+  @ViewChild('newProducerForm') producerForm: NgForm;
+  producer: Producer;
+  address: string;
+
   constructor(server: ServerService) {
-    //codigo de prueba para meter un registro de productor en la lista de requests de productores del server
-    //server.addProducerRequest({ id: 0, name: 'producer1' });
-    //console.log(server.producerRequests());
+    // codigo de prueba para meter un registro de productor en la lista de requests de productores del server
+    // server.addProducerRequest({ id: 0, name: 'producer1' });
+    // console.log(server.producerRequests());
   }
 
   ngOnInit(): void {}
+
+  onSubmit(){
+    // tslint:disable-next-line:max-line-length
+    this.address = this.producerForm.value.location.newProducerProvince + ', ' +
+                   this.producerForm.value.location.newProducerCanton + ', ' +
+                   this.producerForm.value.location.newProducerDistrict;
+
+    this.producer = new Producer(
+      this.producerForm.value.newProducerID,
+      this.producerForm.value.fullName.newProducerName,
+      this.producerForm.value.fullName.newProducerLastName,
+      this.address,
+      this.producerForm.value.newProducerBirthday,
+      this.producerForm.value.newProducerPhone,
+      this.producerForm.value.newProducerSINPE,
+      this.producerForm.value.newProducerDeliveryLocation,
+      this.producerForm.value.newProducerUsername,
+      this.producerForm.value.newProducerPassword
+    );
+
+    console.log(this.producer);
+  }
 }
