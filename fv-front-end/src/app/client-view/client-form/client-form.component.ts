3f7bd063dca16aede3059c5fd643ae7719cdfb6e
Álvaro Vargas Molina <36602950+AlvaroVargasM@users.noreply.github.com>
2020-10-21 04:23:20 -0700

    Made the client-form.component work with the ng forms

--- fv-front-end/src/app/client-view/client-form/client-form.component.ts
+++ fv-front-end/src/app/client-view/client-form/client-form.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {Client} from '../../models/client.model';
+import {Producer} from '../../models/producer.model';
 
 @Component({
   selector: 'client-form',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-form.component.css']
 })
 export class ClientFormComponent implements OnInit {
+  @ViewChild('newClientForm') clientForm: NgForm;
+  client: Client;
+  address;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmitClient(){
+    this.address = this.clientForm.value.location.newClientProvince + ', ' +
+      this.clientForm.value.location.newClientCanton + ', ' +
+      this.clientForm.value.location.newClientDistrict;
+
+    this.client = new Client(
+      this.clientForm.value.newClientID,
+      this.clientForm.value.fullName.newClientName,
+      this.clientForm.value.fullName.newClientLastName,
+      this.address,
+      this.clientForm.value.newClientBirthday,
+      this.clientForm.value.newClientPhone,
+      this.clientForm.value.newClientUsername,
+      this.clientForm.value.newClientPassword
+    );
+
+    console.log(this.client);
   }
 
 }
