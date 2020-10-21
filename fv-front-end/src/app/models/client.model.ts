4442d2cbeca9b8ad12880e634fef6f10a374db00
Álvaro Vargas Molina <36602950+AlvaroVargasM@users.noreply.github.com>
2020-10-21 04:21:31 -0700

    Added a model for the client

---
+++ fv-front-end/src/app/models/client.model.ts
@@ -0,0 +1,10 @@
+export class Client {
+  constructor(public id: number,
+              public name: string,
+              public lastName: string,
+              public address: string,
+              public birthday: any,
+              public phone: number,
+              public user: string,
+              public password: string){}
+}
