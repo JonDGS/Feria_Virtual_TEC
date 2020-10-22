3553e1e0f5b1ceb85ac64d2da8eaa714aeea5505
Álvaro Vargas Molina <36602950+AlvaroVargasM@users.noreply.github.com>
2020-10-21 22:01:57 -0700

    Improved the form for adding new categories

--- fv-front-end/src/app/admin-view/category-management/category-list-changes/category-create/category-create.component.ts
+++ fv-front-end/src/app/admin-view/category-management/category-list-changes/category-create/category-create.component.ts
@@ -1,6 +1,7 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {ServerService} from '../../../../server.service';
 import {Category} from '../../../../models/category.model';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-category-create',
@@ -8,7 +9,7 @@ import {Category} from '../../../../models/category.model';
   styleUrls: ['./category-create.component.css']
 })
 export class CategoryCreateComponent implements OnInit {
-  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('newCategoryForm') categoryForm: NgForm;
 
   constructor(private serverService: ServerService) { }
 
@@ -16,9 +17,15 @@ export class CategoryCreateComponent implements OnInit {
   }
 
   onAddCategory(){
-    const catName = this.nameInputRef.nativeElement.value;
+    const catName = this.categoryForm.value.newCategoryName;
     const catID = this.serverService.categoryID;
     const newCategory = new Category(catName, catID);
+
     this.serverService.addCategory(newCategory);
+
+    console.log(newCategory);
+
+    this.categoryForm.reset();
   }
 }
+
